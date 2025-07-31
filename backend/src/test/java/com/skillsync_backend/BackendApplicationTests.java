24436f015b88907e139b2d4fa7d6542b327765ef
koooolaaaa <125712030+koooolaaaa@users.noreply.github.com>
2025-07-30 19:02:16 -0700

    scaffolded spring boot backend 2

--- backend/src/test/java/com/skillsync_backend/BackendApplicationTests.java
+++ backend/src/test/java/com/skillsync_backend/BackendApplicationTests.java
@@ -1,13 +1,13 @@
-package com.skillsync.backend;
+package com.skillsync_backend;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class SkillsyncBackendApplicationTests {
+class BackendApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
