1672db09a94cbadddbbac1abe877316950cdfef0
koooolaaaa <125712030+koooolaaaa@users.noreply.github.com>
2025-08-02 14:33:22 -0700

    feat(auth): implement AuthController with login and register endpoints

---
+++ backend/src/main/java/com/skillsync_backend/controller/AuthController.java
@@ -0,0 +1,30 @@
+package com.skillsync_backend.controller;
+
+import com.skillsync_backend.dto.AuthRequest;
+import com.skillsync_backend.dto.AuthResponse;
+import com.skillsync_backend.service.UserService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/auth")                              //All endpoints will start with /api/auth
+public class AuthController {
+
+    private final UserService userService;
+
+    @PostMapping("/register")                             //Maps POST /register to register() method
+    public ResponseEntity<AuthResponse>                     //Returns structured HTTP + JSON responses
+    register(@Valid @RequestBody AuthRequest request){      //Reads and validates request body
+        AuthResponse response = userService.register(request);
+        return ResponseEntity.ok(response);
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<AuthResponse> login(@Valid @RequestBody AuthRequest request){
+        AuthResponse response = userService.authenticate(request);
+        return ResponseEntity.ok(response);
+    }
+}
