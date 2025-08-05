b70d62fdbb2c2d002555177a3dcc93f4a4e4751e
koooolaaaa <125712030+koooolaaaa@users.noreply.github.com>
2025-08-04 17:16:39 -0700

    feat(security): configure SecurityFilterChain with JWT authentication

--- backend/src/main/java/com/skillsync_backend/config/SecurityConfig.java
+++ backend/src/main/java/com/skillsync_backend/config/SecurityConfig.java
@@ -1,15 +1,44 @@
 package com.skillsync_backend.config;
 
+import com.skillsync_backend.security.JwtAuthFilter;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
 
 @Configuration
+@EnableWebSecurity
 public class SecurityConfig {
 
+    private final JwtAuthFilter jwtAuthFilter;
+
     @Bean
-    public PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
+    public SecurityFilterChain SecurityFilterChain(HttpSecurity http) throws Exception {
+        return http
+                .csrf(csrf -> csrf.disable())           //Disable CSRF since we're using JWT for auth
+                .authorizeHttpRequests(auth -> auth     //Allow requests to our public endpoints
+                        .requestMatchers("api/auth/**").permitAll()     // public: login/register
+                        .anyRequest().authenticated()                   // all other endpoints require auth
+                )
+                .sessionManagement(session -> session       // Stateless session management (JWT = no session)
+                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                )
+                .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class)         // Add custom JWT filter before the built in username/passwors filter
+                .build();
     }
+
+    // Required for authentication in login process
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration config) throws Exception {
+        return config.getAuthenticationManager();
+    }
+
 }
