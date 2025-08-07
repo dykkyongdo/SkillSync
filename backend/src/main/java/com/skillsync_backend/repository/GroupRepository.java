3c939dbb85d5523de444ac76b13f01b4eacb3849
koooolaaaa <125712030+koooolaaaa@users.noreply.github.com>
2025-08-07 15:50:34 -0700

    feat(repository): add GroupRepository for DB access to groups

---
+++ backend/src/main/java/com/skillsync_backend/repository/GroupRepository.java
@@ -0,0 +1,9 @@
+package com.skillsync_backend.repository;
+
+import com.skillsync_backend.model.Group;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.UUID;
+
+public class GroupRepository {
+}
