5290e626145f89449bd90f52c2851f3e2e311fb4
koooolaaaa <125712030+koooolaaaa@users.noreply.github.com>
2025-08-07 16:09:04 -0700

    feat(dto): add GroupRequest DTO for creating learning groups

---
+++ backend/src/main/java/com/skillsync_backend/dto/GroupRequest.java
@@ -0,0 +1,11 @@
+package com.skillsync_backend.dto;
+
+import lombok.Setter;
+import lombok.Getter;
+
+@Getter
+@Setter
+public class GroupRequest {
+    private String name;
+    private String description;
+}
