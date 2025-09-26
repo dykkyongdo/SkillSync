c212f6389e354fdc601b5bb3fe4f20e4be74ce7a
koooolaaaa <125712030+koooolaaaa@users.noreply.github.com>
2025-09-26 15:07:02 -0700

    feat(layout): include Navbar

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { AuthProvider } from "@/contexts/AuthContext";
+import Navbar from "./components/Navbar";
 import "./globals.css";
 
 export const metadata: Metadata = {
@@ -11,6 +12,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }){
   return (
     <html lang="en">
       <body className="antialiased">
+        <Navbar />
         <AuthProvider>{children}</AuthProvider>
       </body>
     </html>
