ca559f15cce37e681a60557d7f3aa5d69b9415cc
koooolaaaa <125712030+koooolaaaa@users.noreply.github.com>
2025-10-05 16:08:03 -0700

    feat(exceptions): create specific custom exceptions to replace the generic RuntimeException usage

---
+++ backend/src/main/java/com/skillsync_backend/exception/InvalidCredentialsException.java
@@ -0,0 +1,7 @@
+package com.skillsync_backend.exception;
+
+public class InvalidCredentialsException extends RuntimeException {
+    public InvalidCredentialsException(String message) {
+        super(message);
+    }
+}
